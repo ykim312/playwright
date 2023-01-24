1ad6c2a25f1e0107293350672c87bd3198152ee8
ykim312 <ykim312@my.bcit.ca>
2023-01-24 15:06:06 -0800

    adding playwrite

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+export default function About(){
+    return(
+        <>About Page
+        <div>
+            <Link href="/">Go Back</Link>
+        </div>
+        </>
+    )
+}
---
+++ pages/grades.js
@@ -0,0 +1,5 @@
+export default function Grades(){
+    return(
+        <>Grades Page</>
+    )
+}
